82489aaaab6b3898d94fa1348c393c8e1fc20f66
Luis <luis.esculapio@live.com>
2021-09-07 20:00:03 -0300

    feat: add highlightCard on dashboard

--- src/Dashboard/index.tsx
+++ src/Dashboard/index.tsx
@@ -1,13 +1,46 @@
 import React from "react";
-import { Text } from "react-native";
-import { Container, Header } from "./styles";
+
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+    HighlightCards,
+} from "./styles";
+import { TouchableOpacity } from "react-native";
+import Highlight from "../components/HighlightCard";
 
 const Dashboard = () => {
     return (
         <Container>
             <Header>
-                <Text>Luis</Text>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: "https://github.com/luisescx.png" }}
+                        />
+
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Luis</UserName>
+                        </User>
+                    </UserInfo>
+                    <TouchableOpacity>
+                        <Icon name="power" />
+                    </TouchableOpacity>
+                </UserWrapper>
             </Header>
+
+            <HighlightCards>
+                <Highlight />
+                <Highlight />
+                <Highlight />
+            </HighlightCards>
         </Container>
     );
 };
--- src/Dashboard/styles.ts
+++ src/Dashboard/styles.ts
@@ -1,4 +1,7 @@
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
+import { Feather } from "@expo/vector-icons";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const Container = styled.View`
     flex: 1;
@@ -7,6 +10,69 @@ export const Container = styled.View`
 
 export const Header = styled.View`
     width: 100%;
-    height: 278px;
+    height: ${RFPercentage(42)}px;
+
     background-color: ${({ theme }) => theme.colors.primary};
+
+    justify-content: center;
+    align-items: flex-start;
+    /* align-items: center; */
+    flex-direction: row;
+`;
+
+export const UserWrapper = styled.View`
+    width: 100%;
+
+    padding: 0 24px;
+    margin-top: ${getStatusBarHeight() + RFValue(28)}px;
+
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const UserInfo = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const Photo = styled.Image`
+    width: ${RFValue(48)}px;
+    height: ${RFValue(48)}px;
+
+    border-radius: 10px;
+`;
+
+export const User = styled.View`
+    margin-left: 17px;
+`;
+
+export const UserGreeting = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(18)}px;
+    line-height: ${RFValue(20)}px;
+    color: ${({ theme }) => theme.colors.shape};
+`;
+
+export const UserName = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.bold};
+    font-size: ${RFValue(18)}px;
+    line-height: ${RFValue(20)}px;
+    color: ${({ theme }) => theme.colors.shape};
+`;
+
+export const Icon = styled(Feather)`
+    color: ${({ theme }) => theme.colors.secondary};
+    font-size: ${RFValue(24)}px;
+`;
+
+export const HighlightCards = styled.ScrollView.attrs({
+    horizontal: true,
+    showsHorizontalScrollIndicator: false,
+    contentContainerStyle: { paddingHorizontal: 24 },
+})`
+    width: 100%;
+
+    position: absolute;
+    margin-top: ${RFPercentage(20)}px;
 `;
