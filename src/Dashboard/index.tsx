1c69e7e907a51dd99394b3c4b52583b830a06128
Luis <luis.esculapio@live.com>
2021-09-08 00:02:03 -0300

    feat: add transactions on dashboard

--- src/Dashboard/index.tsx
+++ src/Dashboard/index.tsx
@@ -11,11 +11,54 @@ import {
     UserName,
     Icon,
     HighlightCards,
+    Transactions,
+    Title,
+    TransactionCards,
 } from "./styles";
 import { TouchableOpacity } from "react-native";
-import Highlight from "../components/HighlightCard";
+import HighlightCard from "../components/HighlightCard";
+import TransactionCard, { Transaction } from "../components/TransactionCard";
 
 const Dashboard = () => {
+    const transactions = [
+        {
+            id: "1",
+            type: "positive",
+            title: "Desenvolvimento de site",
+            amount: "R$ 12.000,00",
+            category: {
+                key: "salary",
+                name: "Vendas",
+                icon: "dollar-sign",
+            },
+            date: "13/04/2021",
+        },
+        {
+            id: "2",
+            type: "negative",
+            title: "Hamburgueria",
+            amount: "R$ 50,00",
+            category: {
+                key: "salary",
+                name: "Alimentação",
+                icon: "coffee",
+            },
+            date: "13/04/2021",
+        },
+        {
+            id: "3",
+            type: "negative",
+            title: "Aluguel Apto",
+            amount: "R$ 1.200,00",
+            category: {
+                key: "salary",
+                name: "Casa",
+                icon: "shopping-bag",
+            },
+            date: "13/04/2021",
+        },
+    ] as Transaction[];
+
     return (
         <Container>
             <Header>
@@ -37,10 +80,37 @@ const Dashboard = () => {
             </Header>
 
             <HighlightCards>
-                <Highlight />
-                <Highlight />
-                <Highlight />
+                <HighlightCard
+                    type="up"
+                    title={"Entradas"}
+                    amount={"R$ 17.400,00"}
+                    lastTransaction={"Última entrada dia 13 de abril"}
+                />
+                <HighlightCard
+                    type="down"
+                    title={"Saídas"}
+                    amount={"R$ 1.259,00"}
+                    lastTransaction={"Última entrada dia 13 de abril"}
+                />
+                <HighlightCard
+                    type="total"
+                    title={"Total"}
+                    amount={"R$ 16.141,00"}
+                    lastTransaction={"01 à 16 de abril"}
+                />
             </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+
+                <TransactionCards
+                    data={transactions}
+                    keyExtractor={(item) => item.id!}
+                    renderItem={({ item }) => (
+                        <TransactionCard transaction={item} />
+                    )}
+                />
+            </Transactions>
         </Container>
     );
 };
--- src/Dashboard/styles.ts
+++ src/Dashboard/styles.ts
@@ -1,7 +1,12 @@
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 import { Feather } from "@expo/vector-icons";
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import {
+    getBottomSpace,
+    getStatusBarHeight,
+} from "react-native-iphone-x-helper";
+import { FlatList } from "react-native";
+import { Transaction } from "../components/TransactionCard";
 
 export const Container = styled.View`
     flex: 1;
@@ -75,4 +80,23 @@ export const HighlightCards = styled.ScrollView.attrs({
 
     position: absolute;
     margin-top: ${RFPercentage(20)}px;
+`;
+
+export const Transactions = styled.View`
+    flex: 1%;
+
+    padding: 0 24px;
+    margin-top: ${RFPercentage(15)}px;
+`;
+
+export const TransactionCards = styled(
+    FlatList as new () => FlatList<Transaction>
+).attrs({
+    showsVerticalScrollIndicator: false,
+    contentContainerStyle: { paddingBottom: getBottomSpace() + RFValue(16) },
+})``;
+
+export const Title = styled.Text`
+    font-size: ${RFValue(18)}px;
+    font-family: ${({ theme }) => theme.fonts.regular};
 `;
